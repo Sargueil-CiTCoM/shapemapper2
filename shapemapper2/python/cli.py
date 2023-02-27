ba3488343a7d1706fd60e486e4a36a76051ebe70
François-Xavier Lyonnet du Moutier <fx.du.moutier@gmail.com>
2023-02-27 16:52:38 +0100

    transforming to package

--- shapemapper2/python/cli.py
+++ shapemapper2/python/cli.py
@@ -15,37 +15,44 @@ import pyshapemap.pipeline_arg_parser as ap
 from pyshapemap.pipeline_builder import build_pipeline
 from pyshapemap.util import Logger, timestamp, version
 from pyshapemap.flowchart import draw_flowchart
+
 sys.setrecursionlimit(3000)
+
+
 def run(args):
     assert isinstance(args, list)
     try:
-        ap.check_help_version(args[1:]) # print version or usage and exit if those args provided
-        ap.check_no_whitespace(args[1:]) # check for disallowed whitespace characters in input arguments
+        ap.check_help_version(
+            args[1:]
+        )  # print version or usage and exit if those args provided
+        ap.check_no_whitespace(
+            args[1:]
+        )  # check for disallowed whitespace characters in input arguments
         logpath, rest_args = ap.get_log_path(args[1:])
 
-        log = Logger(filepath=logpath,
-                     stream=sys.stdout)
+        log = Logger(filepath=logpath, stream=sys.stdout)
         ts = timestamp()
         term_width, term_height = shutil.get_terminal_size()
-        print("\n" + '#' * term_width)
-        print("\nStarted ShapeMapper v{} at {}\nOutput will be logged to {}".format(version(),
-                                                                                    ts,
-                                                                                    logpath))
-        log.file.write("\n" + '#' * term_width)
-        log.file.write("\nStarted ShapeMapper v{} at {}\n".format(version(),
-                                                                  ts))
+        print("\n" + "#" * term_width)
+        print(
+            "\nStarted ShapeMapper v{} at {}\nOutput will be logged to {}".format(
+                version(), ts, logpath
+            )
+        )
+        log.file.write("\n" + "#" * term_width)
+        log.file.write("\nStarted ShapeMapper v{} at {}\n".format(version(), ts))
         # override stdout and stderr globally to redirect through logger
-        sys.stdout=log
-        sys.stderr=log
+        sys.stdout = log
+        sys.stderr = log
 
-        print("Running from directory: "+os.getcwd())
+        print("Running from directory: " + os.getcwd())
 
         msg = "args: "
         for arg in args[1:]:
             if len(arg) > 100:
-                msg += ' '+arg[:100]+'...'
+                msg += " " + arg[:100] + "..."
             else:
-                msg += ' '+arg
+                msg += " " + arg
         print(msg)
 
         pipeline, arg_dict = ap.construct(rest_args)
@@ -53,18 +60,15 @@ def run(args):
 
         if pipeline.render_flowchart:
             p = pipeline.flowchart_path
-            s = draw_flowchart(pipeline,
-                               p,
-                               path=pipeline.temp,
-                               highlight_dir=pipeline.out)
+            s = draw_flowchart(
+                pipeline, p, path=pipeline.temp, highlight_dir=pipeline.out
+            )
             if s:
-                print("Rendered pipeline flowchart to {} at {}".format(p,
-                                                                       timestamp()))
+                print("Rendered pipeline flowchart to {} at {}".format(p, timestamp()))
             else:
                 msg = "Attempted to render pipeline flowchart to {} at {}, "
                 msg += "but graphviz gave a warning or error."
-                msg = msg.format(p,
-                                 timestamp())
+                msg = msg.format(p, timestamp())
                 print(msg)
 
         success = pipeline.run()
@@ -74,9 +78,13 @@ def run(args):
                 if pipeline.any_star_segfault():
                     # rerun with STAR --genomeSAindexNbase set to defined value (default 3)
                     g = arg_dict["rerun_genomeSAindexNbase"]
-                    print("\nSTAR segfault detected. Rerunning pipeline with --genomeSAindexNbase {} and --overwrite.\n".format(g))
+                    print(
+                        "\nSTAR segfault detected. Rerunning pipeline with --genomeSAindexNbase {} and --overwrite.\n".format(
+                            g
+                        )
+                    )
                     term_width, term_height = shutil.get_terminal_size()
-                    print("\n" + '-' * term_width)
+                    print("\n" + "-" * term_width)
                     new_args = arg_dict
                     new_args["genomeSAindexNbase"] = g
                     new_args["overwrite"] = True
@@ -90,15 +98,20 @@ def run(args):
         if success:
             failed_targets = pipeline.get_failed_targets()
             if len(failed_targets) == 0:
-                print("ShapeMapper run successfully completed at {}.".format(timestamp()))
+                print(
+                    "ShapeMapper run successfully completed at {}.".format(timestamp())
+                )
             else:
                 print("ShapeMapper run completed at {}.".format(timestamp()))
                 if len(failed_targets) == 1:
-                    msg = "WARNING: This RNA has a possible poor quality reactivity profile: "+failed_targets[0]
+                    msg = (
+                        "WARNING: This RNA has a possible poor quality reactivity profile: "
+                        + failed_targets[0]
+                    )
                     print(msg)
                 else:
                     msg = "WARNING: These RNAs have possible poor quality reactivity profiles: "
-                    msg += ', '.join(failed_targets)
+                    msg += ", ".join(failed_targets)
                     print(msg)
                 print("See quality control checks above for details.")
 
@@ -116,7 +129,7 @@ def run(args):
                 print(msg)
 
             # FIXME: don't show this warning if tiled amplicon primer pairs used
-            if max(pipeline.target_lengths) > 800 and pipeline.random_primer_len==0:
+            if max(pipeline.target_lengths) > 800 and pipeline.random_primer_len == 0:
                 msg = "WARNING: no random primer length was specified, "
                 msg += "but at least one RNA is longer than a typical "
                 msg += "directed-primer amplicon. If analyzing a randomly "
@@ -137,6 +150,10 @@ def run(args):
         except (NameError, AttributeError):
             pass
 
-if __name__ == "__main__":
+
+def main_wrapper():
     run(sys.argv)
 
+
+if __name__ == "__main__":
+    main_wrapper()
